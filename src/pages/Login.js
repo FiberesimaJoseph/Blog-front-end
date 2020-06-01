751db571a135ad81c5f45fd4044e7888f10f1664
Tekena <tekipeps@localhost.localdomain>
2020-06-01 09:55:22 +0100

    little fixes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Login() {
   const [input, setInput] = useState({});
@@ -29,9 +30,9 @@ export default function Login() {
         <div className="col-12 col-md-6 px-4">
           <ul className="nav">
             <li className="nav-item">
-              <a className="nav-link active" href="#s">
+              <Link to="/" className="nav-link active">
                 <img className="img-fluid" src="/logo.svg" alt="logo" />
-              </a>
+              </Link>
             </li>
             <li className="nav-item ml-auto mt-2">
               <a className="nav-link" href="/about">
@@ -58,7 +59,6 @@ export default function Login() {
                   <input
                     type="text"
                     className="form-control"
-                    placeholder="Email"
                     name="email"
                     onChange={handleFormInput}
                     id="email"
@@ -70,7 +70,6 @@ export default function Login() {
                     type="password"
                     className="form-control"
                     name="password"
-                    placeholder="password"
                     onChange={handleFormInput}
                     id="password"
                   />
@@ -80,50 +79,52 @@ export default function Login() {
                   ></div>
                 </div>
                 <div className="col-12">
-                  <button className="btn btn-default text-orangered ml-5">
-                    <span>
-                      <svg
-                        version="1.1"
-                        id="Capa_1"
-                        height="60"
-                        width="30"
-                        fill="orangered"
-                        xmlns="http://www.w3.org/2000/svg"
-                        xlink="http://www.w3.org/1999/xlink"
-                        x="0px"
-                        y="0px"
-                        viewBox="0 0 512 512"
-                        space="preserve"
-                      >
-                        <g>
+                  <Link to="/register">
+                    <button className="btn btn-default text-orangered ml-5">
+                      <span>
+                        <svg
+                          version="1.1"
+                          id="Capa_1"
+                          height="60"
+                          width="30"
+                          fill="orangered"
+                          xmlns="http://www.w3.org/2000/svg"
+                          xlink="http://www.w3.org/1999/xlink"
+                          x="0px"
+                          y="0px"
+                          viewBox="0 0 512 512"
+                          space="preserve"
+                        >
                           <g>
-                            <path
-                              d="M492,236H68.442l70.164-69.824c7.829-7.792,7.859-20.455,0.067-28.284c-7.792-7.83-20.456-7.859-28.285-0.068
+                            <g>
+                              <path
+                                d="M492,236H68.442l70.164-69.824c7.829-7.792,7.859-20.455,0.067-28.284c-7.792-7.83-20.456-7.859-28.285-0.068
                         l-104.504,104c-0.007,0.006-0.012,0.013-0.018,0.019c-7.809,7.792-7.834,20.496-0.002,28.314c0.007,0.006,0.012,0.013,0.018,0.019
                         l104.504,104c7.828,7.79,20.492,7.763,28.285-0.068c7.792-7.829,7.762-20.492-0.067-28.284L68.442,276H492
                         c11.046,0,20-8.954,20-20C512,244.954,503.046,236,492,236z"
-                            />
+                              />
+                            </g>
                           </g>
-                        </g>
-                        <g></g>
-                        <g></g>
-                        <g></g>
-                        <g></g>
-                        <g></g>
-                        <g></g>
-                        <g></g>
-                        <g></g>
-                        <g></g>
-                        <g></g>
-                        <g></g>
-                        <g></g>
-                        <g></g>
-                        <g></g>
-                        <g></g>
-                      </svg>
-                    </span>
-                    Sign Up
-                  </button>
+                          <g></g>
+                          <g></g>
+                          <g></g>
+                          <g></g>
+                          <g></g>
+                          <g></g>
+                          <g></g>
+                          <g></g>
+                          <g></g>
+                          <g></g>
+                          <g></g>
+                          <g></g>
+                          <g></g>
+                          <g></g>
+                          <g></g>
+                        </svg>
+                      </span>
+                      Sign Up
+                    </button>
+                  </Link>
                   <button type="submit" className="btn btn-orange px-5 ml-5">
                     Sign In
                   </button>
