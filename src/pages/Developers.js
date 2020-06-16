cc201bdd678fb404669e64241781752bb66f539d
FiberesimaJoseph <sctbilly@gmail.com>
2020-06-16 09:11:30 -0600

    modified developer section

--- src/pages/Developers.js
+++ src/pages/Developers.js
@@ -6,14 +6,18 @@ const devs = [
   {
     name: "Joseph Fiberesima",
     github: "https://github.com/FiberesimaJoseph",
-    image:
-      "https://avatars1.githubusercontent.com/u/52601802?s=400&u=e75b956f9dcc7b9333132f81d53196396cbfe162&v=4",
+    twitter: "https://twitter.com/JosephFiberesi1",
+    linkedin: "https://www.linkedin.com/in/joseph-fiberesima-607777193/",
+    gmail: "josephfiberesima@gmail.com",
+    image:  "https://avatars1.githubusercontent.com/u/52601802?s=400&u=e75b956f9dcc7b9333132f81d53196396cbfe162&v=4",
   },
   {
     name: "Tekena Solomon",
     github: "https://github.com/Tekipeps",
-    image:
-      "https://avatars1.githubusercontent.com/u/45674928?s=400&u=d6d145e9d73ad380868bcbb5822c1593ea43662a&v=4",
+    twitter: "https://twitter.com/tekipeps",
+    linkedin: "https://www.linkedin.com/in/tekena-solomon-49668519b/",
+    gmail: "tekena157@gmail.com",
+    image: "https://avatars1.githubusercontent.com/u/45674928?s=400&u=d6d145e9d73ad380868bcbb5822c1593ea43662a&v=4",
   },
 ];
 
@@ -21,21 +25,31 @@ const Developers = () => {
   return (
     <Jumbotron fluid>
       <Container className="developers">
-        <h1>Developers</h1>
-        <div>
+        <h1>About the Developers</h1>
           {devs.map((dev, i) => {
             return (
               <div key={i} className="developer-detail">
                 <img src={dev.image} alt=""></img>
                 <span>{dev.name}</span>
                 <div>
-                  Github
+                  Github:
                   <Link to={dev.github}>{dev.github}</Link>
                 </div>
+                <div> 
+                  Twitter:
+                  <Link to={dev.twitter}>{dev.twitter}</Link>
+                </div>
+                <div>
+                  Linkedin:
+                  <Link to={dev.linkedin}>{dev.linkedin}</Link>
+                  </div>
+                  <div>
+                      Gmail:
+                      <Link to={dev.gmail}>{dev.gmail}</Link>
+                    </div>
               </div>
             );
           })}
-        </div>
       </Container>
     </Jumbotron>
   );
